7e1112789ecbe86607cd2444b69a687076289bcd
Shuyawa89 <shuyaiwamoto@gmail.com>
2025-05-24 18:04:14 +0900

    add: DetailからListにしてみた

--- src/llm-translate.tsx
+++ src/llm-translate.tsx
@@ -1,5 +1,18 @@
-import { Detail } from "@raycast/api";
+import { List } from "@raycast/api";
 
 export default function Command() {
-  return <Detail markdown="# Hello World" />;
+  return (
+    <List>
+      <List.Item
+        title="日本語→英語"
+        subtitle="こんにちは→Hello"
+        icon="🇯🇵"
+      />
+      <List.Item 
+        title="英語→日本語"
+        subtitle="Hello→こんにちは"
+        icon="🇺🇸"
+      />
+    </List>
+  );
 }
