f089b8e225d120746be9933b7868a135f379b403
Shuyawa89 <shuyaiwamoto@gmail.com>
2025-06-01 00:53:23 +0900

    ref: メインコンポーネントを切り分け

--- src/llmTranslate.tsx
+++ src/llmTranslate.tsx
@@ -1,8 +1,7 @@
-import { List, Action, ActionPanel, Detail } from "@raycast/api";
 import { useTranslation } from "./hooks/useTranslation";
-import { getSelectedTextSafely } from "./utils/selectedTextUtils";
-import { TRANSLATION_ACTIONS } from "./utils/translationActions";
 import { ManualInputForm } from "./components/ManualInputForm";
+import { TranslationResultView } from "./components/TranslationResultView";
+import { TranslationActionList } from "./components/TranslationActionList";
 
 export default function Command() {
   const {
@@ -17,16 +16,7 @@ export default function Command() {
 
   // 結果があった場合の画面
   if (translationResult) {
-    return (
-      <Detail
-        markdown={translationResult}
-        actions={
-          <ActionPanel>
-            <Action title="リストに戻る" onAction={resetTranslation} />
-          </ActionPanel>
-        }
-      />
-    );
+    return <TranslationResultView markdown={translationResult} onBack={resetTranslation} />;
   }
 
   if (isInputForm) {
@@ -36,38 +26,11 @@ export default function Command() {
           handleTranslate("自動判定", text); //翻訳実行
           hideManualInput();
         }}
-        onCancel={() => {
-          hideManualInput();
-        }}
+        onCancel={hideManualInput}
       />
     );
   }
   return (
-    <List isLoading={isLoading}>
-      {TRANSLATION_ACTIONS.map((action) => (
-        <List.Item
-          key={action.id}
-          title={action.title}
-          subtitle={action.subtitle}
-          icon={action.icon}
-          actions={
-            <ActionPanel>
-              <Action
-                title="翻訳実行"
-                onAction={async () => {
-                  if (action.type === "selected-text") {
-                    const selectedText = getSelectedTextSafely();
-                    const text = await selectedText;
-                    handleTranslate("自動判定", text || "");
-                  } else if (action.type === "manual-text") {
-                    showManualInput(); // 入力フォームを表示
-                  }
-                }}
-              />
-            </ActionPanel>
-          }
-        />
-      ))}
-    </List>
+    <TranslationActionList isLoading={isLoading} onTranslate={handleTranslate} onShowManualInput={showManualInput} />
   );
 }
